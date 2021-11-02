4dd1d739ffc17567317f51f35b4986b5532e9fa1
Daniel <dbuehler1@my.wctc.edu>
2021-11-02 17:17:42 -0500

    Missing Dependencies

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="searchBar">
+
+    <div class="search input-group mb-3 ">
+
+      <label class="input-group-text" for="search"><i class="fas fa-search"></i></label>
+      <input class="form-control col-md-2" id="search" type="text" v-model="searchTerm">
+      <button class="btn btn-primary" type="button" @click="$emit('searchList', searchTerm.toLowerCase())">Search</button>
+    </div>
+    <h1 id="inventoryHeader">Inventory</h1><br><br>
+
+  </div>
+
+
+</template>
+
+<script>
+
+export default {
+name: "SearchBar",
+  data(){
+    return{
+      searchTerm: '',
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.searchBar{
+
+
+}
+</style>
---
+++ src/components/Song.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="song">
+    <img :src="item.img" class="card-img-top">
+    <p>Audio</p>
+    <h3 class="card-title">{{item.title}}</h3>
+    <p class="card-text">Artist: {{item.artist}}</p>
+    <p class="card-text">{{item.type}}</p>
+
+  </div>
+</template>
+
+<script>
+import {Song} from "@/models/LibraryItems";
+
+export default {
+name: "Song",
+  props: {
+    item: Song
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
